187e683fc1e98e12d823ce90fa4cb44a67d197d9
Triserden <triserden@gmail.com>
2024-04-03 10:49:03 +0200

    7 current   2024-04-03 10:46:04  23.11.20240328.219951b   6.1.82                          *

--- hosts/lenovolaptop/configuration.nix
+++ hosts/lenovolaptop/configuration.nix
@@ -76,6 +76,7 @@
     pkgs.neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     pkgs.wget
     pkgs.gcc_multi
+    pkgs.cargo
   ];
 
   # Enable Flakes
