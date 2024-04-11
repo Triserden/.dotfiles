bde9b0da7a0d0844061231c0f41faaa6ea104eb1
Triserden <triserden@gmail.com>
2024-04-11 16:09:03 +0200

    14 current  2024-04-11 16:09:01  23.11.20240328.219951b   6.1.82                          *

--- hosts/lenovolaptop/configuration.nix
+++ hosts/lenovolaptop/configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       outputs.nixModules.hyprland
+      outputs.nixModules.nwmanager
     ];
 
   nix.settings = {
@@ -23,15 +24,7 @@
   boot.loader.grub.useOSProber = true;
 
   networking.hostName = "lenovolaptop"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
+  
   # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
 
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,3 +1,16 @@
 {
+  # Core modules
+  nwmanager = import ./nwmanager.nix; 
   hyprland = import ./hyprland.nix;
+  firefox = import ./firefox.nix;
+  gpg = import ./gpg.nix;
+  git = import ./git.nix;
+  wget = import ./wget.nix;
+
+  # Shell replacements
+  bat = import ./bat.nix; # cat replacement
+  nushell = import ./nushell.nix; # shell replacement
+  alacritty = import ./alacritty.nix;
+  eza = import ./exa/nix; #ls replacement
+
 }
