16485a30007ad1e2d33d7265a01d2b531b9816b3
Triserden <triserden@gmail.com>
2024-06-09 00:10:09 +0200

    56 current  2024-06-09 00:10:01  23.11.20240525.9d29cd2  6.1.91                          *

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -8,7 +8,7 @@
 
     extraConfig = {
       commit.gpgsign = true;
-      user.signingkey = "2422C7EE03C629FD998BF923FD5CD86B6562D6B7";
+      user.signingkey = "46CC369C9264F713";
     };
   };
 }
