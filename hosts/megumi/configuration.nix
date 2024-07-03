257f6a974bdfd51ac34b21fcf6fdce8b819de080
Triserden <triserden@gmail.com>
2024-07-03 15:39:00 +0200

    77 current  2024-07-03 15:38:52  23.11.20240525.9d29cd2  6.1.91                          *

--- hosts/megumi/configuration.nix
+++ hosts/megumi/configuration.nix
@@ -52,6 +52,21 @@
   nixpkgs.config.permittedInsecurePackages = [
   ];
 
+  programs.nix-ld.enable = true;
+
+  # Sets up all the libraries to load
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    zlib
+    fuse3
+    icu
+    nss
+    openssl
+    curl
+    expat
+    # ...
+  ];
+
   sops.defaultSopsFile = ./secrets.yaml;
  
   sops.secrets.id_ed25519 = {
