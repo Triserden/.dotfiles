e45ffd33acc1b874b18e3e773da1241793b752d6
Triserden <triserden@gmail.com>
2024-07-13 20:00:36 +0200

    105 current  2024-07-13 20:00:27  23.11.20240525.9d29cd2  6.1.91                          *

--- modules/nixos/games.nix
+++ modules/nixos/games.nix
@@ -6,6 +6,6 @@
     winetricks
     # native wayland support (unstable)
     wineWowPackages.waylandFull
-    (unstable.prismlauncher.override { additionalLibs = [nss nspr mesa alsa-lib alsa-lib.dev]; })
+    (unstable.prismlauncher.override { additionalLibs = [nss nspr mesa alsa-lib ]; })
   ];
 }
