664265118f1953c53c40481aba5efb90b4d34014
Triserden <triserden@gmail.com>
2024-07-26 15:57:48 +0200

    128 current  2024-07-14 10:54:59  23.11.20240525.9d29cd2  6.1.91                          *

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,10 +1,19 @@
-
+{config, ...}:
 {
   programs.git = {
     enable = true;
     lfs.enable = true;
     userName = "Triserden";
     userEmail = "triserden@gmail.com";
+    
+    includes = [{
+      condition = "gitdir:~/Code/spc/"; # Trailing slash is important!
+      contents = {
+        user.name = config.sops.spcUser;
+        user.email = config.sops.spcEmail;
+        user.signingkey = config.sops.spcGPGKey;
+  };
+}];
 
     extraConfig = {
       commit.gpgsign = true;
