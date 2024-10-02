488df9636b32f46404b688910d4ae28dfd9ae687
Triserden <triserden@gmail.com>
2024-10-02 22:23:22 +0200

    193 current  2024-10-02 22:23:11  24.05.20240922.23cbb25  6.6.52                          *

--- modules/nixos/rust.nix
+++ modules/nixos/rust.nix
@@ -10,5 +10,10 @@
       pkgs.devenv
     ];
     programs.direnv.enable = true;
+  
+    nix.settings = {
+      substituters = ["https://devenv.cachix.org"];
+      trusted-public-keys = ["devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="];
+    };
   };
 }
