8b90c036a5335df8845dce7214351e1d926d6eea
Triserden <triserden@gmail.com>
2024-11-10 15:34:17 +0100

    209 current  2024-11-10 15:26:51  24.05.20240930.1719f27  6.6.52                          *

--- hosts/megumi/configuration.nix
+++ hosts/megumi/configuration.nix
@@ -33,7 +33,7 @@
     pkgs.unstable.obsidian
    
     # TODO: Make mail declarative
-    pkgs.thunderbird-bin
+    pkgs.unstable.thunderbird-latest-unwrapped
 
     # TODO: Move to module
     pkgs.kicad
