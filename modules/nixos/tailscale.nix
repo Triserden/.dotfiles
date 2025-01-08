5099ca36ae5681e4096e45d12df0c0283e7489b1
Triserden <triserden@gmail.com>
2025-01-08 22:18:56 +0100

    237 current  2025-01-07 10:20:32  24.11.20241213.314e12b  6.6.64                          *

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -20,6 +20,11 @@
       allowedUDPPorts = [ config.services.tailscale.port ];
     };
 
+    boot.kernel.sysctl = {
+        "net.ipv4.ip_forward" = 1;
+        "net.ipv6.conf.all.forwarding" = 1;
+      };
+
     systemd.services.tailscale-autoconnect = {
       description = "Automatic connection to Tailscale";
 
