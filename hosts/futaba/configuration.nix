0f3c8244f8b96d854fd274c4c8d0bb48db5e0020
Triserden <triserden@gmail.com>
2025-01-08 16:21:50 +0100

    237 current  2025-01-07 10:20:32  24.11.20241213.314e12b  6.6.64                          *

--- hosts/futaba/configuration.nix
+++ hosts/futaba/configuration.nix
@@ -51,7 +51,7 @@
       pterodactyl_env = {
         sopsFile = ./pterodactylconfig.yaml;
         format = "yaml";
-        path = "/home/triserden/services/pterodacty/panel/config.yaml";
+        path = "/home/triserden/services/pterodacty/wings/config.yaml";
         owner="triserden";
         key = "";
       };
