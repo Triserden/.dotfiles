eeb62a95c5cbe16b744b9eb79fd292e2378d0382
Triserden <triserden@gmail.com>
2025-02-17 13:31:22 +0100

    258 current  2025-02-17 13:31:09  24.11.20250213.0b73e36  6.6.76                          *

--- modules/home/gui.nix
+++ modules/home/gui.nix
@@ -9,7 +9,7 @@
     ## TODO: Move to dedicated firefox module
 
     textfox = {
-        enable = true;
+        enable = false;
         profile = "default";
         config = {
         # Optional config
