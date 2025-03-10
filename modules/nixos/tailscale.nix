460e703933209530d79fa6fd9c2c25adab78f344
Triserden <triserden@gmail.com>
2025-03-10 12:36:52 +0100

    266 current  2025-02-28 11:58:56  24.11.20250213.0b73e36  6.6.76                          *

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -50,6 +50,7 @@
         # Check if key is almost expired
         if (( $(date --date $(${tailscale}/bin/tailscale status --json | ${jq}/bin/jq '.Self.KeyExpiry' | tr -d '"') +'%s') > $(date --date "30 days ago" +'%s') ))
         then
+          echo "Force reauthenticating tailscale"
           ${tailscale}/bin/tailscale up --force-reauth --authkey=$(cat "${config.tailscale.authkey}") --ssh 
           exit 0
         fi
