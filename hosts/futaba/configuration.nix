4558e1a3428963ab9f614362629653d9738d6282
Triserden <triserden@gmail.com>
2025-03-23 01:49:43 +0100

    268 current  2025-03-19 21:10:02  24.11.20250318.da04445  6.6.83                          *

--- hosts/futaba/configuration.nix
+++ hosts/futaba/configuration.nix
@@ -75,7 +75,7 @@ in
         sopsFile = ./secrets/pterodactyl_wing_config.yaml;
         format = "yaml";
         path = "/data/pterodactyl/wings/etc/pterodactyl/config.yml";
-        owner="988";
+        owner="triserden";
         group="users";
         key = "";
       };
