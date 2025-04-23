28822916bef5c5c5d0003314fb323101f85d9332
Triserden <triserden@gmail.com>
2025-04-23 15:14:50 +0200

    268 current  2025-03-19 21:10:02  24.11.20250318.da04445  6.6.83                          *

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -7,7 +7,6 @@
   config = lib.mkIf config.hyprland.enable {
     programs.hyprland = { # we use this instead of putting it in systemPackages/users  
       enable = true;  
-      xwayland.enable = true;  
     };
 
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
