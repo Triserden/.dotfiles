f35ca9a1a54b9a78613576a933e0c7473ad456d8
Triserden <triserden@gmail.com>
2025-04-23 15:08:51 +0200

    268 current  2025-03-19 21:10:02  24.11.20250318.da04445  6.6.83                          *

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -8,7 +8,7 @@
     programs.hyprland = { # we use this instead of putting it in systemPackages/users  
       enable = true;  
       xwayland.enable = true;  
-      enableNvidiaPatches = lib.mkIf config.nvidia.enable true; # ONLY use this line if you have an nvidia card  
+      enableNvidiaPatches = lib.mkIf (config.nvidia.enable == true) true; # ONLY use this line if you have an nvidia card  
     };
 
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
