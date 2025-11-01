86d9107da43a11185706c540d733df4b269d9c68
Triserden <triserden@gmail.com>
2025-11-01 19:58:50 +0100

    feat: fully automatic tailscale connection on fresh install

--- hosts/common/core/tailscale.nix
+++ hosts/common/core/tailscale.nix
@@ -6,6 +6,17 @@
     enable = true;
     openFirewall = false;
     authKeyFile = config.sops.secrets."tailscale-oauth-key".path;
-    extraUpFlags = ["--ssh" "--advertise-tags=tag:end-user"];
+    authKeyParameters = {
+      ephemeral = false;
+    };
+    extraUpFlags = ["--ssh" "--advertise-tags=tag:end-devices"];
+  };
+  systemd.services.tailscaled-autoconnect = {
+    wants = ["network-online.target"];
+    after = ["network-online.target"];
+    serviceConfig = {
+      Restart = "on-failure";
+      RestartSec = "15";
+    };
   };
 }
